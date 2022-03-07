dee82e034f8a6e35228ead83c32f5acf2680146a
Eduardo Tonatto <edtonatto@gmail.com>
2022-03-07 10:28:54 -0300

    :bug: Ajustado endpoint de edição repositório

--- src/index.js
+++ src/index.js
@@ -30,8 +30,9 @@ app.post("/repositories", (request, response) => {
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
+  console.log(updatedRepository)
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
